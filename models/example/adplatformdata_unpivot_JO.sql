96eeafd25fab2832b371ac52deda97e3cf952def
josephoprism <joseph.obu@prism.training>
2025-02-13 10:03:16 +0000

    adplatformdata_unpivot_JO.sql

--- models/example/adplatformdata_unpivot_JO.sql
+++ models/example/adplatformdata_unpivot_JO.sql
@@ -1,42 +1,48 @@
-WITH adplatform_data AS (
-    SELECT 
-        date,
-        traffic_source,
-        session_id,
-        user_crm_id,
-        SUM(cost) AS total_cost,
-        SUM(clicks) AS total_clicks,
-        SUM(impressions) AS total_impressions,
-        COUNT(DISTINCT session_id) AS total_sessions,
-        COUNT(DISTINCT user_crm_id) AS total_new_customers
-    FROM 
-        {{ ref('adplatformdata_unpivot_JO') }}
-    WHERE
-        traffic_medium = 'cpc'
-    GROUP BY 
-        date, traffic_source, session_id, user_crm_id
-),
-funnelevents AS (
-    SELECT
-        session_id,
-        COUNT(DISTINCT transaction_id) AS total_conversions
-    FROM
-        {{ source('prism_acquire', 'funnelevents') }}
-    GROUP BY session_id
-)
-SELECT 
-    a.date,
-    a.traffic_source,
-    SUM(a.total_cost) AS total_cost,
-    SUM(a.total_clicks) AS total_clicks,
-    SUM(a.total_impressions) AS total_impressions,
-    SUM(a.total_sessions) AS total_sessions,
-    SUM(f.total_conversions) AS total_transactions,
-    SUM(a.total_new_customers) AS total_new_customers,
-    (SUM(a.total_clicks) / SUM(a.total_impressions)) * 100 AS click_through_rate,
-    (SUM(f.total_conversions) / SUM(a.total_new_customers)) * 100 AS conversion_rate,
-    (SUM(a.total_cost) / SUM(a.total_clicks)) AS cost_per_click,
-    (SUM(a.total_cost) / SUM(a.total_new_customers)) AS cost_per_acquisition,
-    (SUM(a.total_cost) / SUM(a.total_sessions)) AS cost_per_session
-FROM 
-    adplatform_data AS a
+{{ config(
+    materialized='table'
+) }}
+
+select 
+    date,
+    'criteo' as traffic_source,
+    criteo_cost as cost,
+    criteo_clicks as clicks,
+    criteo_impressions as impressions
+from 
+    {{ source('prism_acquire', 'adplatform_data') }}
+union all
+select 
+    date,
+    'google' as traffic_source,
+    google_cost as cost,
+    google_clicks as clicks,
+    google_impressions as impressions
+from 
+    {{ source('prism_acquire', 'adplatform_data') }}
+union all
+select 
+    date,
+    'meta' as traffic_source,
+    meta_cost as cost,
+    meta_clicks as clicks,
+    meta_impressions as impressions
+from 
+    {{ source('prism_acquire', 'adplatform_data') }}
+union all
+select 
+    date,
+    'rtbhouse' as traffic_source,
+    rtbhouse_cost as cost,
+    rtbhouse_clicks as clicks,
+    rtbhouse_impressions as impressions
+from 
+    {{ source('prism_acquire', 'adplatform_data') }}
+union all
+select 
+    date,
+    'tiktok' as traffic_source,
+    tiktok_cost as cost,
+    tiktok_clicks as clicks,
+    tiktok_impressions as impressions
+from 
+    {{ source('prism_acquire', 'adplatform_data') }}
